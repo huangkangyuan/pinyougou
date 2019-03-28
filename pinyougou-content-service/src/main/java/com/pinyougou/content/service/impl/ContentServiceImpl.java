098ebf97a771d505c54d534d4ef11b6eb801e16f
root <614772276@qq.com>
2019-03-28 08:07:53 +0800

    content_service

---
+++ pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -0,0 +1,112 @@
+package com.pinyougou.content.service.impl;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.content.service.ContentService;
+import com.pinyougou.mapper.TbContentMapper;
+import com.pinyougou.pojo.TbContent;
+import com.pinyougou.pojo.TbContentExample;
+import com.pinyougou.pojo.TbContentExample.Criteria;
+
+import entity.PageResult;
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class ContentServiceImpl implements ContentService {
+
+	@Autowired
+	private TbContentMapper contentMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbContent> findAll() {
+		return contentMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbContent content) {
+		contentMapper.insert(content);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbContent content){
+		contentMapper.updateByPrimaryKey(content);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbContent findOne(Long id){
+		return contentMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			contentMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbContentExample example=new TbContentExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(content!=null){			
+						if(content.getTitle()!=null && content.getTitle().length()>0){
+				criteria.andTitleLike("%"+content.getTitle()+"%");
+			}
+			if(content.getUrl()!=null && content.getUrl().length()>0){
+				criteria.andUrlLike("%"+content.getUrl()+"%");
+			}
+			if(content.getPic()!=null && content.getPic().length()>0){
+				criteria.andPicLike("%"+content.getPic()+"%");
+			}
+			if(content.getStatus()!=null && content.getStatus().length()>0){
+				criteria.andStatusLike("%"+content.getStatus()+"%");
+			}
+	
+		}
+		
+		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+	
+}
