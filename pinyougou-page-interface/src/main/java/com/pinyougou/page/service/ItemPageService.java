a4bc2b04128acd470d37e01df7a426ae4ccb1a3b
root <614772276@qq.com>
2019-04-01 08:04:44 +0800

    freemarker

---
+++ pinyougou-page-interface/src/main/java/com/pinyougou/page/service/ItemPageService.java
@@ -0,0 +1,13 @@
+package com.pinyougou.page.service;
+
+public interface ItemPageService {
+
+	/**
+	 * 生成商品详细页
+	 * 
+	 * @param goodsId
+	 * @return
+	 */
+	public boolean genItemHtml(Long goodsId);
+
+}
---
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -0,0 +1,96 @@
+package com.pinyougou.page.service.impl;
+
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import com.pinyougou.mapper.TbGoodsDescMapper;
+import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.page.service.ItemPageService;
+import com.pinyougou.pojo.TbGoods;
+import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import com.pinyougou.pojo.TbItemExample.Criteria;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+@Service
+public class ItemPageServiceImpl implements ItemPageService {
+
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
+
+	@Value("${pageDir}")
+	private String pageDir;
+
+	@Autowired
+	private TbGoodsMapper goodsMapper;
+
+	@Autowired
+	private TbGoodsDescMapper goodsDescMapper;
+
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+
+	@Autowired
+	private TbItemMapper itemMapper;
+
+	@Override
+	public boolean genItemHtml(Long goodsId) {
+
+		Configuration configuration = freeMarkerConfigurer.getConfiguration();
+
+		try {
+			Template template = configuration.getTemplate("item.ftl");
+			// 创建数据模型
+			Map dataModel = new HashMap<>();
+			// 1.商品主表数据
+			TbGoods goods = goodsMapper.selectByPrimaryKey(goodsId);
+			dataModel.put("goods", goods);
+			// 2.商品扩展表数据
+			TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
+			dataModel.put("goodsDesc", goodsDesc);
+			// 3.读取商品分类
+
+			String itemCat1 = itemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+			String itemCat2 = itemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+			String itemCat3 = itemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+			dataModel.put("itemCat1", itemCat1);
+			dataModel.put("itemCat2", itemCat2);
+			dataModel.put("itemCat3", itemCat3);
+
+			// 4.读取SKU列表
+			TbItemExample example = new TbItemExample();
+			Criteria criteria = example.createCriteria();
+			criteria.andGoodsIdEqualTo(goodsId);// SPU ID
+			criteria.andStatusEqualTo("1");// 状态有效
+			example.setOrderByClause("is_default desc");// 按是否默认字段进行降序排序，目的是返回的结果第一条为默认SKU
+
+			List<TbItem> itemList = itemMapper.selectByExample(example);
+			dataModel.put("itemList", itemList);
+
+			Writer out = new FileWriter(pageDir + goodsId + ".html");
+
+			template.process(dataModel, out);// 输出
+			out.close();
+			return true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+}
