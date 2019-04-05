21a87a5d45b2b36155484105f03e70e87eaf37d7
root <614772276@qq.com>
2019-04-05 13:02:17 +0800

    user_interface

---
+++ pinyougou-user-interface/src/main/java/com/pinyougou/user/service/UserService.java
@@ -0,0 +1,76 @@
+package com.pinyougou.user.service;
+import java.util.List;
+import com.pinyougou.pojo.TbUser;
+
+import entity.PageResult;
+/**
+ * 服务层接口
+ * @author Administrator
+ *
+ */
+public interface UserService {
+
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	public List<TbUser> findAll();
+	
+	
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
+	
+	
+	/**
+	 * 增加
+	*/
+	public void add(TbUser user);
+	
+	
+	/**
+	 * 修改
+	 */
+	public void update(TbUser user);
+	
+
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	public TbUser findOne(Long id);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long [] ids);
+
+	/**
+	 * 分页
+	 * @param pageNum 当前页 码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbUser user, int pageNum,int pageSize);
+	
+	
+	/**
+	 *生成/发送短信验证码
+	 * @param phone
+	 */
+	public void createSmsCode(String phone);
+	
+	/**
+	 * 校验验证码
+	 * @param phone
+	 * @param code
+	 * @return
+	 */
+	public boolean checkSmsCode(String phone,String code);
+	
+}
