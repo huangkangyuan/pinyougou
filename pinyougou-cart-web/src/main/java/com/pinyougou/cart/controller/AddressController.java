faa716c0bc148e0012ae71dd5a7261a24870d8b8
root <614772276@qq.com>
2019-04-08 18:23:40 +0800

    cart_web

---
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/AddressController.java
@@ -0,0 +1,122 @@
+package com.pinyougou.cart.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.pinyougou.pojo.TbAddress;
+import com.pinyougou.user.service.AddressService;
+
+import entity.PageResult;
+import entity.Result;
+/**
+ * controller
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+	@Autowired
+	private AddressService addressService;
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findAll")
+	public List<TbAddress> findAll(){			
+		return addressService.findAll();
+	}
+	
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult  findPage(int page,int rows){			
+		return addressService.findPage(page, rows);
+	}
+	
+	/**
+	 * 增加
+	 * @param address
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbAddress address){
+		try {
+			addressService.add(address);
+			return new Result(true, "增加成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "增加失败");
+		}
+	}
+	
+	/**
+	 * 修改
+	 * @param address
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbAddress address){
+		try {
+			addressService.update(address);
+			return new Result(true, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "修改失败");
+		}
+	}	
+	
+	/**
+	 * 获取实体
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbAddress findOne(Long id){
+		return addressService.findOne(id);		
+	}
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long [] ids){
+		try {
+			addressService.delete(ids);
+			return new Result(true, "删除成功"); 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "删除失败");
+		}
+	}
+	
+		/**
+	 * 查询+分页
+	 * @param brand
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(@RequestBody TbAddress address, int page, int rows  ){
+		return addressService.findPage(address, page, rows);		
+	}
+	
+	@RequestMapping("/findListByLoginUser")
+	public List<TbAddress> findListByLoginUser(){
+		//获取登陆用户
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		return addressService.findListByUserId(username);		
+	}
+	
+}
---
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/OrderController.java
@@ -0,0 +1,121 @@
+package com.pinyougou.cart.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.pinyougou.pojo.TbOrder;
+import com.pinyougou.order.service.OrderService;
+
+import entity.PageResult;
+import entity.Result;
+/**
+ * controller
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	private OrderService orderService;
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findAll")
+	public List<TbOrder> findAll(){			
+		return orderService.findAll();
+	}
+	
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult  findPage(int page,int rows){			
+		return orderService.findPage(page, rows);
+	}
+	
+	/**
+	 * 增加
+	 * @param order
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbOrder order){
+		
+		//获取当前登录人账号
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		order.setUserId(username);
+		order.setSourceType("2");//订单来源  PC
+		
+		try {
+			orderService.add(order);
+			return new Result(true, "增加成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "增加失败");
+		}
+	}
+	
+	/**
+	 * 修改
+	 * @param order
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbOrder order){
+		try {
+			orderService.update(order);
+			return new Result(true, "修改成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "修改失败");
+		}
+	}	
+	
+	/**
+	 * 获取实体
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbOrder findOne(Long id){
+		return orderService.findOne(id);		
+	}
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long [] ids){
+		try {
+			orderService.delete(ids);
+			return new Result(true, "删除成功"); 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "删除失败");
+		}
+	}
+	
+		/**
+	 * 查询+分页
+	 * @param brand
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(@RequestBody TbOrder order, int page, int rows  ){
+		return orderService.findPage(order, page, rows);		
+	}
+	
+}
