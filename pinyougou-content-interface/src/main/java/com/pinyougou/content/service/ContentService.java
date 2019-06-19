bf5babf190fa86d25aeea160205da206e851ae83
root <614772276@qq.com>
2019-06-19 10:58:40 +0800

    log

--- pinyougou-content-interface/src/main/java/com/pinyougou/content/service/ContentService.java
+++ pinyougou-content-interface/src/main/java/com/pinyougou/content/service/ContentService.java
@@ -64,5 +64,12 @@ public interface ContentService {
 	 * @return
 	 */
 	public PageResult findPage(TbContent content, int pageNum, int pageSize);
+	
+	/**
+	 * 根据广告分类ID查询广告列表
+	 * @param categoryId
+	 * @return
+	 */
+	public List<TbContent> findByCategoryId(Long categoryId);
 
 }
