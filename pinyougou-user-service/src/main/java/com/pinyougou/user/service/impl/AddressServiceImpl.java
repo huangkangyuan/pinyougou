fe8ee4e38ee8e5472a645a566c16b9c338cc527d
root <614772276@qq.com>
2019-06-23 17:32:02 +0800

    用户接口

---
+++ pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/AddressServiceImpl.java
@@ -0,0 +1,137 @@
+package com.pinyougou.user.service.impl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbAddressMapper;
+import com.pinyougou.pojo.TbAddress;
+import com.pinyougou.pojo.TbAddressExample;
+import com.pinyougou.pojo.TbAddressExample.Criteria;
+import com.pinyougou.user.service.AddressService;
+
+import entity.PageResult;
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class AddressServiceImpl implements AddressService {
+
+	@Autowired
+	private TbAddressMapper addressMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbAddress> findAll() {
+		return addressMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbAddress> page=   (Page<TbAddress>) addressMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbAddress address) {
+		addressMapper.insert(address);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbAddress address){
+		addressMapper.updateByPrimaryKey(address);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbAddress findOne(Long id){
+		return addressMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			addressMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbAddress address, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbAddressExample example=new TbAddressExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(address!=null){			
+						if(address.getUserId()!=null && address.getUserId().length()>0){
+				criteria.andUserIdLike("%"+address.getUserId()+"%");
+			}
+			if(address.getProvinceId()!=null && address.getProvinceId().length()>0){
+				criteria.andProvinceIdLike("%"+address.getProvinceId()+"%");
+			}
+			if(address.getCityId()!=null && address.getCityId().length()>0){
+				criteria.andCityIdLike("%"+address.getCityId()+"%");
+			}
+			if(address.getTownId()!=null && address.getTownId().length()>0){
+				criteria.andTownIdLike("%"+address.getTownId()+"%");
+			}
+			if(address.getMobile()!=null && address.getMobile().length()>0){
+				criteria.andMobileLike("%"+address.getMobile()+"%");
+			}
+			if(address.getAddress()!=null && address.getAddress().length()>0){
+				criteria.andAddressLike("%"+address.getAddress()+"%");
+			}
+			if(address.getContact()!=null && address.getContact().length()>0){
+				criteria.andContactLike("%"+address.getContact()+"%");
+			}
+			if(address.getIsDefault()!=null && address.getIsDefault().length()>0){
+				criteria.andIsDefaultLike("%"+address.getIsDefault()+"%");
+			}
+			if(address.getNotes()!=null && address.getNotes().length()>0){
+				criteria.andNotesLike("%"+address.getNotes()+"%");
+			}
+			if(address.getAlias()!=null && address.getAlias().length()>0){
+				criteria.andAliasLike("%"+address.getAlias()+"%");
+			}
+	
+		}
+		
+		Page<TbAddress> page= (Page<TbAddress>)addressMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	@Override
+	public List<TbAddress> findListByUserId(String userId) {
+		
+		TbAddressExample example=new TbAddressExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andUserIdEqualTo(userId);
+		return addressMapper.selectByExample(example);
+	}
+	
+}
